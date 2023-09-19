4cf00e861b2a41a36ad2eb6fe8a790893d940694
ajraarup <83853119+ajraarup@users.noreply.github.com>
2023-09-18 22:41:10 -0400

    Update LodgingContent.java

--- CLMS_GUI/LodgingContent.java
+++ CLMS_GUI/LodgingContent.java
@@ -1,22 +1,16 @@
-import javafx.scene.control.Button;
-import javafx.scene.control.DatePicker;
-import javafx.scene.control.Spinner;
-import javafx.scene.control.TextField;
-import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
+import javafx.scene.web.WebEngine;
+import javafx.scene.web.WebView;
 
 public class LodgingContent extends ContentArea {
 	@Override
 	public void initialize() {
 		
-        TextField destinationField = new TextField("TEST");
-        DatePicker departingDate = new DatePicker();
-        Spinner<Integer> durationSpinner = new Spinner<>(1, 30, 1);
-        Spinner<Integer> travelersSpinner = new Spinner<>(1, 100, 1);
-        Button searchButton = new Button("Search");
-
-        HBox inputFields = new HBox(10); 
-        inputFields.getChildren().addAll(destinationField, departingDate, durationSpinner, travelersSpinner, searchButton);
-		content = new VBox(inputFields);
+        WebView webView = new WebView();
+        WebEngine webEngine = webView.getEngine();
+        // Load a web page
+        webEngine.load("https://www.cruisemapper.com/cruise-search"); // add in lodging site
+        
+		content = new VBox(webView);
 	}
 }
