c86192198726b9efaa6145877476120dc586b069
Daniel <72395566+DJcloon@users.noreply.github.com>
2023-09-25 11:40:32 +0900

    Update CLMS.java

--- CLMS/CLMS.java
+++ CLMS/CLMS.java
@@ -1,8 +1,21 @@
 package CLMS;
 
+import java.io.IOException;
+
 public class CLMS {
 
-	public CLMS() {
+	DataLoader dataLoader = new DataLoader();
+	static CruiseShip[] cruiseShip = new CruiseShip[60];
+
+	public static void loadExcel(String filename) {
+		for(int i = 0; i < cruiseShip.length; i++) 
+		{
+			try {
+				cruiseShip[i] = DataLoader.loadShip(filename, i);
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
 	}
 
 }
