c203bbbc8f0fcf22f9bfea75c6113f8f6ba909d3
Daniel <72395566+DJcloon@users.noreply.github.com>
2023-09-27 07:06:06 +0900

    Update Cabin.java

--- CLMS/Cabin.java
+++ CLMS/Cabin.java
@@ -1,14 +1,37 @@
 package CLMS;
 
+import java.util.Arrays;
+
 public class Cabin{
 
 	Boolean occupied = false;
 	String amenities;
 	int cabinType;
-	
-	public Cabin(String amenities, int cabinType) {
+	String[][] passengers;
+
+	public Cabin(String amenities, int cabinType, String[][] cabinPasengers) {
 		this.amenities = amenities;
 		this.cabinType = cabinType;
+		@SuppressWarnings("unused")
+		String[][] passengers = new String[cabinPasengers.length][cabinPasengers[0].length];
+	}
+
+	public int findPassenger(String name) {
+		for(int i=0; i<passengers.length; i++)
+		{
+			for(int j=0; j<passengers[i].length; j++)
+			{
+				if(passengers[i][j] == name) {
+					return CruiseShip.getShipID();
+				} 
+			}
+		}
+		return -1;
+	}
+
+	public void getPassengers() {
+		/*test print*/
+		System.out.println(Arrays.deepToString(passengers));
 	}
 	
 	public String getAmenities() {return amenities;}
