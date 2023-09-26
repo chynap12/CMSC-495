1f2609aee77a29f13cca18367963ba5b807ed4d7
Daniel <72395566+DJcloon@users.noreply.github.com>
2023-09-27 07:06:34 +0900

    Update CruiseShip.java

--- CLMS/CruiseShip.java
+++ CLMS/CruiseShip.java
@@ -3,7 +3,7 @@ package CLMS;
 public class CruiseShip extends CruiseCompany {
 
 	//variables
-	private int shipID;
+	private static int shipID;
 	private String name;
 	private String location;
 	private String origin;
@@ -21,7 +21,7 @@ public class CruiseShip extends CruiseCompany {
 	public CruiseShip(int shipID, String company, String name, String location, String origin, String destination,
 			String[] itinerary,	int tripLength,	int numCabins, int yearOfBuild, int maintenance, int maxCapacity) {
 		super(company);
-		this.shipID = shipID;
+		this.setShipID(shipID);
 		this.name = name;
 		this.location = location;
 		this.origin = origin;
@@ -35,22 +35,51 @@ public class CruiseShip extends CruiseCompany {
 	}
 	
 	//getter methods
-	public int getShipID(){return shipID;}
-	public String getCompany(){return company;}
-	public String getName(){return name;}
-	public String getLocation(){return location;}
-	public String getorigin(){return origin;}
-	public String getDestination(){return destination;}
-	public String[] getItinerary(){return itinerary;}
-	public int getTripLength(){return tripLength;}
-	public int getYearOfBuild(){return numCabins;}
-	public int getNumCabins(){return yearOfBuild;}
+	public void setShipID(int shipID){
+		CruiseShip.shipID = shipID;
+		}
+	public static int getShipID(){
+		return shipID;
+		}
+	public String getCompany(){
+		return company;
+		}
+	public String getName(){
+		return name;
+		}
+	public String getLocation(){
+		return location;
+		}
+	public String getorigin(){
+		return origin;
+		}
+	public String getDestination(){
+		return destination;
+		}
+	public String[] getItinerary(){
+		return itinerary;
+		}
+	public int getTripLength(){
+		return tripLength;
+		}
+	public int getYearOfBuild(){
+		return yearOfBuild;
+		}
+	public int getNumCabins(){
+		return numCabins;
+		}
 	public int getMaintenance() {
 		return maintenance;
-	}
+		}
 	public void setMaintenance(int maintenance) {
 		this.maintenance = maintenance;
 	}
-	public int getMaxCapacity(){return maxCapacity;}
+	public int getMaxCapacity(){
+		return maxCapacity;
+		}
+
+	public int getAvailableCabins() {
+		return getNumCabins() - cabins.length;	
+		}
 
 }
