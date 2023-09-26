b0d6a6038011c1d3c449c5b827d6b2f74c2206bf
Daniel <72395566+DJcloon@users.noreply.github.com>
2023-09-27 07:09:24 +0900

    Update StandardCabin.java

--- CLMS/StandardCabin.java
+++ CLMS/StandardCabin.java
@@ -2,8 +2,8 @@ package CLMS;
 
 public class StandardCabin extends Cabin{
 	private static int cabinType = 0;
-	public StandardCabin() {
-		super("Porthole, Toilet,", cabinType);
+		public StandardCabin(String[][] cabinPassenger) {
+		super("Porthole, Toilet,", cabinType, cabinPassenger);
 	}
 
 }
