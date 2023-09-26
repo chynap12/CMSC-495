3419848cbd34b734cc19706f59ca8d52f27bf364
Daniel <72395566+DJcloon@users.noreply.github.com>
2023-09-27 07:07:38 +0900

    Update PremiumCabin.java

--- CLMS/PremiumCabin.java
+++ CLMS/PremiumCabin.java
@@ -3,8 +3,8 @@ package CLMS;
 public class PremiumCabin extends Cabin {
 	private static int cabinType = 2;
 	
-	public PremiumCabin() {
-		super("Balcony, Bath, Toilet", cabinType);
+	public PremiumCabin(String[][] cabinPassenger) {
+		super("Balcony, Bath, Toilet", cabinType, cabinPassenger);
 	}
 
 }
